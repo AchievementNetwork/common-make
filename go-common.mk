2b50858f1bd68d37047162f2fd5f2736aa467043
Greg Lewis <glewis@achievementnetwork.org>
2020-12-01 15:18:04 -0800

    IN-234 Simplify updating local copies
    * Fix a README error
    * Add a notice that local copies shouldn't be changed
    * Add recipes for checking for and doing updates to local copies
    * Add instructions for doing this to the README

--- go-common.mk
+++ go-common.mk
@@ -1,3 +1,11 @@
+# This file is part of the ANet Engineering Common Make infrastructure
+#
+# Please do not edit it in place if this is a local copy outside of the common-make
+# repository.  Rather, override the external variable and targets in your Makefile.
+# If you have an enhancement, please submit a pull request against
+#   https://github.com/AchievementNetwork/common-make
+# and then update your local copy once it is approved
+
 ## Variables
 
 ## External variables
@@ -37,6 +45,7 @@ endif
 .PHONY: pre-lint standard-lint post-lint
 .PHONY: pre-test standard-test post-test
 .PHONY: pre-testcover standard-testcover post-testcover
+.PHONY: _checkcommonupdate _commonupdate
 
 ## External targets
 # These may be overridden and used in repo Makefiles
@@ -94,6 +103,23 @@ pre-testcover::
 standard-testcover:: coverage.html
 
 post-testcover::
+
+# External targets that can be used, but should not be overridden
+
+_checkcommonupdate::
+	@TMP_FILE=`mktemp go-common.mk.XXXXXX`; \
+		curl -o $${TMP_FILE} -s -S -L https://raw.github.com/AchievementNetwork/common-make/main/go-common.mk; \
+		if [ $$? -ne 0 ]; then echo "Update check failed"; exit 1; fi; \
+		diff -b --brief go-common.mk $${TMP_FILE} > /dev/null; \
+		if [ $$? -ne 0 ]; then echo "It looks like go-common.mk is out of date.  Please run \`make _commonupdate\`"; else echo "go-common.mk up to date"; fi; \
+		$(RM) $${TMP_FILE}
+
+_commonupdate::
+	@TMP_FILE=`mktemp go-common.mk.XXXXXX`; \
+		curl -o $${TMP_FILE} -s -S -L https://raw.github.com/AchievementNetwork/common-make/main/go-common.mk; \
+		if [ $$? -ne 0 ]; then echo "Update failed"; exit 1; fi; \
+		mv $${TMP_FILE} go-common.mk; \
+		echo "Please test and then commit the new version of go-common.mk"
 
 ## Internal targets
 # DO NOT OVERRIDE OR USE
